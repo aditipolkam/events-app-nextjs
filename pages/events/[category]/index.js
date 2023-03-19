f92f35c4d7f05a2e5f2c04922f40427c9b237a25
aditipolkam <aditipolkam@gmail.com>
2023-03-19 19:18:18 +0530

    [SETUP] basic routes and pages

---
+++ pages/events/[category]/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Events in London</h1>
+      <a href="/event/event1">Event 1</a>
+    </div>
+  );
+};
+
+export default Page;
---
+++ pages/events/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div>Events in london</div>;
+};
+
+export default Page;
